b71dfc578bfa5d9dca519546453b51fbe0faee50
julija2202 <julietkajagolovich@gmail.com>
2022-11-20 15:50:06 +0300

    hw3_2

--- src/hw/HW3.java
+++ src/hw/HW3.java
@@ -51,6 +51,78 @@ public class HW3 {
         System.out.println(fff + "\n" + ddd);
 
         System.out.println("Task №" + ++number);
+        float floatSum = f + ff;
+        float floatSub = f - ff;
+        float floatProduct = f * ff;
+        float floatQuotient = f / ff;
+        float floatRemainder = f % ff;
+        System.out.println(floatSum + "\n" +
+                floatSub  + "\n" +
+                floatProduct  + "\n" +
+                floatQuotient  + "\n" +
+                floatRemainder);
+
+        System.out.println("Task №" + ++number);
+        double doubleSum = f + ff;
+        double doubleSub = f - ff;
+        double doubleProduct = f * ff;
+        double doubleQuotient = f / ff;
+        double doubleRemainder = f % ff;
+        System.out.println(doubleSum + "\n" +
+                doubleSub + "\n" +
+                doubleProduct  + "\n" +
+                doubleQuotient  + "\n" +
+                doubleRemainder);
+
+        System.out.println("Task №" + ++number);
+        String line3 = "________________________________________________" +
+                "____________________________";
+        System.out.println(line3 + "\n"
+                + left + "f = 00.101101 " + middle + "ff = 30.19999" + "\t\t" + middle +
+                "d = 100.101101" + middle + "dd = 10.09999" + "\t\t\t"+ right + "\n"
+                + line3 + "\n"
+                + left + "floatSum = " + floatSum + "\t\t\t\t\t\t" + middle +
+                "doubleSum = " + doubleSum + "\t\t" + right + "\n"
+                + left + "floatSub = " + floatSub + "\t\t\t\t\t\t" + middle +
+                "doubleSub = " + doubleSub + "\t\t\t"+ right + "\n"
+                + left + "floatProduct = " + floatProduct + "\t\t\t\t" + middle +
+                "doubleProduct = " + doubleProduct + right + "\n"
+                + left + "floatQuotient = " + floatQuotient + "\t\t\t\t" + middle +
+                "doubleQuotient = " + doubleQuotient + right + "\n"
+                + left + "floatRemainder = " + floatRemainder + "\t\t\t\t" +
+                middle + "doubleRemainder = " + doubleRemainder + right + "\n"
+                + line3);
+
+        System.out.println("Task №" + ++number);
+        System.out.println( "..........     ...........     .        .     ..........    " + "\n"
+                                     + "       :    :       :     :        :    :      :     " + "\n"
+                                     + "       :    :.........:     :        :    :........:     " + "\n"
+                                     + ":.......:     :       :      :.......:     :      :     " + "\n");
+
+        System.out.println("Task №" + ++number);
+        Byte t1 = 0;
+        Short t2 = 150;
+        Short t3 = -120;
+        Float t4 = - 505.505f;
+        Integer t5 = 100100;
+        Long t6 = 100010001000l;
+        Double t7 = 2.66666666666666;
+        Double t8 = - 1000000.001;
+        Short t9 = 1010;
+        System.out.println(t1 + ", " +  t2 + ", " + t3 + ", " + t4 + ", " + t5 + ", " + t6
+                + ", " + t7 + ", " + t8 +", " + t9);
+
+        System.out.println("Task №" + ++number);
+
+        System.out.println("Task №" + ++number);
+        Integer num1 = 5;
+        Integer num2 = 5;
+       Boolean bool =  true;
+       bool = num1.equals(num2);
+       System.out.println("Если num1 равно  num2, то результат сравнения методом .equals() = …" +  bool);
+
+        System.out.println("Task №" + ++number);
+         
 
 
 
